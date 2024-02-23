258f0d9b6f94a6480b87207166af8eef710a6c98
SosthenesMS <sosthenesms@gmail.com>
2024-02-23 16:44:53 -0300

    First commit

---
+++ src/main/java/com/consumo/api/httpclient/ConsumoApiHttpClientApplication.java
@@ -0,0 +1,13 @@
+package com.consumo.api.httpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiHttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiHttpClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/consumo/api/httpclient/Entities/BrasilApi.java
@@ -0,0 +1,20 @@
+package com.consumo.api.httpclient.Entities;
+
+import org.springframework.boot.SpringApplication;
+
+import com.consumo.api.httpclient.ConsumoApiHttpClientApplication;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "BRASILAPI")
+public class BrasilApi {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+
+}
