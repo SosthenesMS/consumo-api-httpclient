76ec376f73c0993afe8a49fe31ef793234adf6ea
SosthenesMS <sosthenesms@gmail.com>
2024-02-24 23:49:17 -0300

    Criação do novo projeto de consumo de api com HttpClient e lib Gson

---
+++ Com lib Gson/consumo-api-httpclient-Gson/src/main/java/com/consumo/api/httpclient/Gson/ConsumoApiHttpclientGsonApplication.java
@@ -0,0 +1,13 @@
+package com.consumo.api.httpclient.Gson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiHttpclientGsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiHttpclientGsonApplication.class, args);
+	}
+
+}
