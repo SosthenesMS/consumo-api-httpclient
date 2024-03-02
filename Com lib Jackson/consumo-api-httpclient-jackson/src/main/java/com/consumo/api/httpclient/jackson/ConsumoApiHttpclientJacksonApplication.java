9d0ffb849af25a817f05d8afdc46910f6cd38988
SosthenesMS <sosthenesms@gmail.com>
2024-03-02 11:46:19 -0300

    Criação do projeto jackson

---
+++ Com lib Jackson/consumo-api-httpclient-jackson/src/main/java/com/consumo/api/httpclient/jackson/ConsumoApiHttpclientJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.consumo.api.httpclient.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiHttpclientJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiHttpclientJacksonApplication.class, args);
+	}
+
+}
---
+++ Com lib Jackson/consumo-api-httpclient-jackson/src/test/java/com/consumo/api/httpclient/jackson/ConsumoApiHttpclientJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumo.api.httpclient.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiHttpclientJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
